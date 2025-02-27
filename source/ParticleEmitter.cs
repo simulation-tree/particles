09645ad70180d1b4ba323d290e877c89afe9546a
Phill <philldamaskin@gmail.com>
2025-02-26 21:39:28 -0500

    Create ParticleEmitter.cs

---
+++ source/ParticleEmitter.cs
@@ -0,0 +1,99 @@
+﻿using Collections.Generic;
+using Particles.Components;
+using System;
+using Unmanaged;
+using Worlds;
+
+namespace Particles
+{
+    public readonly partial struct ParticleEmitter : IEntity
+    {
+        public readonly ref IsParticleEmitter.Emission Emission => ref GetComponent<IsParticleEmitter>().emission;
+        public readonly ref IsParticleEmitter.InitialParticleState InitialParticleState => ref GetComponent<IsParticleEmitter>().initialParticleState;
+        public readonly USpan<Particle> AllParticles => GetArray<Particle>().AsSpan();
+        
+        public readonly uint AliveParticles
+        {
+            get
+            {
+                USpan<Particle> particles = GetArray<Particle>().AsSpan();
+                uint count = 0;
+                for (uint i = 0; i < particles.Length; i++)
+                {
+                    if (!particles[i].free)
+                    {
+                        count++;
+                    }
+                }
+
+                return count;
+            }
+        }
+
+        public readonly uint FreeParticles
+        {
+            get
+            {
+                USpan<Particle> particles = GetArray<Particle>().AsSpan();
+                uint count = 0;
+                for (uint i = 0; i < particles.Length; i++)
+                {
+                    if (particles[i].free)
+                    {
+                        count++;
+                    }
+                }
+
+                return count;
+            }
+        }
+
+        public ParticleEmitter(World world)
+        {
+            this.world = world;
+            this.value = world.CreateEntity(IsParticleEmitter.Default);
+            Array<Particle> particles = CreateArray<Particle>(256);
+            for (uint i = 0; i < particles.Length; i++)
+            {
+                particles[i].free = true;
+            }
+        }
+
+        public ParticleEmitter(World world, IsParticleEmitter emitter)
+        {
+            this.world = world;
+            this.value = world.CreateEntity(emitter);
+            Array<Particle> particles = CreateArray<Particle>(256);
+            for (uint i = 0; i < particles.Length; i++)
+            {
+                particles[i].free = true;
+            }
+        }
+
+        readonly void IEntity.Describe(ref Archetype archetype)
+        {
+            archetype.AddComponentType<IsParticleEmitter>();
+            archetype.AddArrayType<Particle>();
+        }
+
+        public readonly ref Particle GetAliveParticle(uint index)
+        {
+            Array<Particle> particles = GetArray<Particle>();
+            uint count = 0;
+            for (uint i = 0; i < particles.Length; i++)
+            {
+                if (!particles[i].free)
+                {
+                    if (count == index)
+                    {
+                        return ref particles[i];
+                    }
+
+                    count++;
+                }
+            }
+
+            throw new IndexOutOfRangeException();
+        }
+    }
+}
