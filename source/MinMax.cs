e6d45c5cace12af743537b60b43b806324c8c4a4
Phill <philldamaskin@gmail.com>
2025-03-01 17:54:57 -0500

    use get span more often

--- source/MinMax.cs
+++ source/MinMax.cs
@@ -103,7 +103,7 @@ namespace Particles
         {
             USpan<char> buffer = stackalloc char[128];
             uint length = ToString(buffer);
-            return buffer.Slice(0, length).ToString();
+            return buffer.GetSpan(length).ToString();
         }
 
         public unsafe readonly uint ToString(USpan<char> destination)
