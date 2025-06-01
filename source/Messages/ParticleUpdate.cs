4a0815829b48809dc56b6d706c4862134987bdf3
Phill <philldamaskin@gmail.com>
2025-06-01 12:35:53 -0400

    add update message

---
+++ source/Messages/ParticleUpdate.cs
@@ -0,0 +1,12 @@
+﻿namespace Particles.Messages
+{
+    public readonly struct ParticleUpdate
+    {
+        public readonly float deltaTime;
+
+        public ParticleUpdate(float deltaTime)
+        {
+            this.deltaTime = deltaTime;
+        }
+    }
+}
